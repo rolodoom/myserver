41b973a129073b56e464db2974990f1f8ab041ad
rolodoom <rolodoom@gmail.com>
2020-08-04 18:15:36 -0500

    Update

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 
 <footer class="text-muted">
+
     <div class="container-fluid p-3 p-md-5">
         <ul class="list-inline">
             <li class="list-inline-item"><a href="https://github.com/rolodoom" target="blank">GitHub</a></li>
@@ -8,10 +9,12 @@
             <li class="list-inline-item"><a href="https://bootswatch.com/" target="blank">Bootswatch</a></li>
         </ul>
         <p>
-            Code released under the MIT License.<br>
+            Code released under the MIT License.<br>Based on 
             Based on <a href="http://getbootstrap.com" target="_blank">Bootstrap</a>. Icons from <a href="https://fontawesome.com" target="_blank">Font Awesome</a>. Theme by <a href="https://github.com/thomaspark/bootswatch" target="_blank">Bootswatch</a>.
         </p>
 
-        
     </div>
+
 </footer>
+
+
