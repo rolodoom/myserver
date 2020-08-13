f2a0fa286df032a238cb469f9998c15bc035f9ac
rolodoom <rolodoom@gmail.com>
2020-08-13 18:41:38 -0500

    Update

--- includes/inc.favicons.php
+++ includes/inc.favicons.php
@@ -1,7 +1,7 @@
 <?php if (preg_match("/inc.favicon.php/", $_SERVER['PHP_SELF'],$match)) {  die("<h4>Forbidden.</h4>"); } ?>
 
         <!-- Favicon v2.2 RoLoDoOm 2020 -->
-        <?php $__faviconurl__ = '__lib__/assets/favicons'; ?>
+        <?php $__faviconurl__ = 'myserver/assets/favicons'; ?>
 
         <!-- Favicon web browsers -->
         <link rel="shortcut icon" href="<?php echo $__faviconurl__; ?>/favicon.ico" type="image/x-icon">
@@ -15,3 +15,4 @@
         <link rel="mask-icon" href="<?php echo $__faviconurl__; ?>/safari-pinned-tab.svg" color="#5bbad5">
         <meta name="msapplication-TileColor" content="#da532c">
         <meta name="theme-color" content="#ffffff">
+[Untitled]
