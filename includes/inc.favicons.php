45a5d77ee7e2f44122fa926618e5734e31dc26cf
rolodoom <rolodoom@gmail.com>
2020-08-15 17:16:30 -0500

    Update

--- includes/inc.favicons.php
+++ includes/inc.favicons.php
@@ -15,4 +15,4 @@
         <link rel="mask-icon" href="<?php echo $__faviconurl__; ?>/safari-pinned-tab.svg" color="#5bbad5">
         <meta name="msapplication-TileColor" content="#da532c">
         <meta name="theme-color" content="#ffffff">
-[Untitled]
+
