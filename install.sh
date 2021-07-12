ae36236c3e120713430ae706318766333998b3f5
rolodoom <rolodoom@gmail.com>
2021-07-12 10:46:17 -0500

    Update

--- install.sh
+++ install.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-cp theme/htaccess-dist.txt ../.htaccess
+
+# Install Apaxy
+unset input
+while [ -z ${input} ]; do
+    echo -n "Enter server directory. Can't be empty: "
+    read input
+done
+sed "s/{server}/$input/g" theme/htaccess-dist.txt > ../.htaccess
+
+# Install npm dependencies
+echo "Installing npm dependencies..."
 npm i
+
+# Creating index.php
+echo "Creating index..."
 cat <<END  >../index.php
 <?php include_once('myserver/start.php'); ?>
 END
-
